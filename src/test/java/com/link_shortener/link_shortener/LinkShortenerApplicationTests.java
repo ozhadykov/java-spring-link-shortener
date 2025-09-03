6829dbdbc9e4da06fcdd36896367284e4baeef62
Omar Zhadykov <omar3282088@gmail.com>
2025-09-03 22:45:34 +0200

    base unconfigured project

---
+++ src/test/java/com/link_shortener/link_shortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link_shortener.link_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
