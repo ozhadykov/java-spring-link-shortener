bc52af1d31de9be3c75ca34073e5f685009cd549
Omar Zhadykov <omar3282088@gmail.com>
2025-10-15 20:24:53 +0200

    formating

--- src/main/java/com/link_shortener/link_shortener/config/SecurityConfig.java
+++ src/main/java/com/link_shortener/link_shortener/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.link_shortener.link_shortener.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,15 +12,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        // Allow anyone to access the home page
-                        .requestMatchers("/").permitAll()
-                        // Any other request must be authenticated
-                        .anyRequest().authenticated()
-                )
-                // This one line enables the entire session/cookie login flow!
-                .formLogin(withDefaults());
+        http.authorizeHttpRequests(
+                auth -> auth
+                // Allow anyone to access the home page
+                .requestMatchers("/").permitAll()
+                // Any other request must be authenticated
+                .anyRequest().authenticated()
+        )
+        // This one line enables the entire session/cookie login flow!
+        .formLogin(withDefaults());
 
         return http.build();
     }
